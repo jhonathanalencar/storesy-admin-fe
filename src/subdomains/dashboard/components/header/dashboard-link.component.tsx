e4afac7f6f69db2b9b9ac7f894a426db7596b686
jhonathanalencar <jhonathan.alencarfs@gmail.com>
2024-04-16 23:20:28 -0300

    feat: add dashboard link component

---
+++ src/subdomains/dashboard/components/header/dashboard-link.component.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import type { ComponentProps } from 'react';
+import Link, { type LinkProps } from 'next/link';
+import { usePathname } from 'next/navigation';
+import { twMerge } from 'tailwind-merge';
+
+type DashboardLinkProps = ComponentProps<'a'> & LinkProps;
+
+export function DashboardLink({
+  href,
+  children,
+  className,
+  ...rest
+}: DashboardLinkProps) {
+  const pathname = usePathname();
+
+  return (
+    <Link
+      href={href}
+      className={twMerge(
+        'flex items-center gap-1 rounded-full px-3 py-1.5 transition-colors duration-200 hover:bg-white/5',
+        className,
+        pathname === href ? 'bg-zinc-700' : null
+      )}
+      {...rest}
+    >
+      {children}
+    </Link>
+  );
+}
