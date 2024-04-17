1e027124b4c42ed7c17e34fddae23ca015051d0d
jhonathanalencar <jhonathan.alencarfs@gmail.com>
2024-04-17 20:32:35 -0300

    test: add dashboard layout tests

---
+++ src/shared/layouts/dashboard.layout.test.tsx
@@ -0,0 +1,40 @@
+import { render, screen } from '@testing-library/react';
+
+import { DashboardLayout } from './dashboard.layout';
+
+describe('<DashboardLayout>', () => {
+  describe('Render', () => {
+    it('should render a main element', () => {
+      const children = <h1>Children</h1>;
+
+      render(<DashboardLayout>{children}</DashboardLayout>);
+
+      const main = screen.getByRole('main');
+
+      expect(main).toBeInTheDocument();
+    });
+
+    it('should render the children prop correctly', () => {
+      const children = <h1>Children</h1>;
+
+      render(<DashboardLayout>{children}</DashboardLayout>);
+
+      const text = screen.getByRole('heading', {
+        level: 1,
+        name: /children/i,
+      });
+
+      expect(text).toBeInTheDocument();
+    });
+
+    it('should render a horizontal line separator', () => {
+      const children = <h1>Children</h1>;
+
+      render(<DashboardLayout>{children}</DashboardLayout>);
+
+      const separator = screen.getByRole('separator');
+
+      expect(separator).toBeInTheDocument();
+    });
+  });
+});
