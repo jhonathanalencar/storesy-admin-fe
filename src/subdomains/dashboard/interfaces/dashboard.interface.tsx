716d6fc93f639901a553a2ba4721ad7cb9fb903e
jhonathanalencar <jhonathan.alencarfs@gmail.com>
2024-04-17 21:20:44 -0300

    feat: add aria label to section on dashboard interface

--- src/subdomains/dashboard/interfaces/dashboard.interface.tsx
+++ src/subdomains/dashboard/interfaces/dashboard.interface.tsx
@@ -1,6 +1,6 @@
 export function DashboardInterface() {
   return (
-    <section>
+    <section aria-label="dashboard welcome">
       <div className="mx-auto flex w-full max-w-6xl justify-center p-2 md:p-4">
         <div className="w-full max-w-[500px] rounded border border-zinc-700 bg-zinc-800 p-4 drop-shadow-md md:p-8">
           <h3 className="text-2xl font-semibold">
