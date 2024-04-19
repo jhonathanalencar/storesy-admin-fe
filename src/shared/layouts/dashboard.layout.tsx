6307eea728a53324ef65993660f269753d19e075
jhonathanalencar <jhonathan.alencarfs@gmail.com>
2024-04-18 22:06:30 -0300

    style: change background color of dashboard layout

--- src/shared/layouts/dashboard.layout.tsx
+++ src/shared/layouts/dashboard.layout.tsx
@@ -2,7 +2,7 @@ import { DashboardHeader } from '@subdomains/dashboard/components/header/dashboa
 
 export function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
-    <main className="flex min-h-full flex-col bg-zinc-900 text-zinc-100">
+    <main className="flex min-h-full flex-col bg-zinc-950 text-zinc-100">
       <DashboardHeader />
       <div role="separator" className="my-3 h-px bg-zinc-700" />
       <div className="flex-1">{children}</div>
