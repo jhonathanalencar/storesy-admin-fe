ebfdaabccef24e78aa55d822320ccade1e1535c0
jhonathanalencar <jhonathan.alencarfs@gmail.com>
2024-04-22 22:17:42 -0300

    style: add focus style to checkbox component

--- src/shared/components/checkbox.component.tsx
+++ src/shared/components/checkbox.component.tsx
@@ -10,7 +10,7 @@ export function Checkbox({ className, ...rest }: CheckboxProps) {
   return (
     <CheckboxPrimitive.Root
       className={twMerge(
-        'flex h-5 w-5 items-center justify-center rounded border border-zinc-700 bg-zinc-800 p-1',
+        'flex h-5 w-5 items-center justify-center rounded border border-zinc-700 bg-zinc-800 p-1 outline-none focus-visible:ring-2 focus-visible:ring-green-400 focus-visible:ring-offset-2 focus-visible:ring-offset-zinc-950',
         className
       )}
       {...rest}
