29a92d545b3fbdca8220c0dfbddacb53a16a1286
jhonathanalencar <jhonathan.alencarfs@gmail.com>
2024-05-13 13:28:34 -0300

    test: add products container tests

---
+++ src/subdomains/dashboard/containers/products.container.test.tsx
@@ -0,0 +1,33 @@
+import { render, screen } from '@testing-library/react';
+
+import { ProductsContainer } from './products.container';
+
+describe('<ProductsContainer>', () => {
+  describe('Render', () => {
+    it('should render a main element', () => {
+      render(<ProductsContainer />);
+
+      const main = screen.getByRole('main');
+
+      expect(main).toBeInTheDocument();
+    });
+
+    it('should render a header element', () => {
+      render(<ProductsContainer />);
+
+      const header = screen.getByRole('banner');
+
+      expect(header).toBeInTheDocument();
+    });
+
+    it('should render the "products" title', () => {
+      render(<ProductsContainer />);
+
+      const title = screen.getByRole('heading', {
+        name: /products/i,
+      });
+
+      expect(title).toBeInTheDocument();
+    });
+  });
+});
