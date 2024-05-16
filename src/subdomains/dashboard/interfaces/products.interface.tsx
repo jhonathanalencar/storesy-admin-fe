b2234be1ade90237a615c294da41616c8b5648a5
jhonathanalencar <jhonathan.alencarfs@gmail.com>
2024-05-16 11:45:29 -0300

    style: style pagination links

--- src/subdomains/dashboard/interfaces/products.interface.tsx
+++ src/subdomains/dashboard/interfaces/products.interface.tsx
@@ -214,58 +214,92 @@ export function ProductsInterface({ products }: ProductsInterfaceProps) {
         ) : null}
       </div>
 
-      <div className="flex items-center justify-end">
-        <span>Page 1 of {totalPages}</span>
-        <div className="flex items-center gap-2">
+      <div className="flex items-center justify-end gap-6 py-4">
+        <span className="text-sm font-medium text-zinc-500 md:text-base">
+          Page 1 of {totalPages}
+        </span>
+        <div role="navigation" className="flex items-center gap-2">
           {isFirstPage ? (
-            <span aria-disabled>
-              <ChevronsLeftIcon focusable={false} aria-hidden />
+            <span
+              aria-disabled
+              className="h-7 w-7 rounded bg-zinc-800 p-1.5 opacity-90 ring-1 ring-inset ring-zinc-700"
+            >
+              <ChevronsLeftIcon
+                focusable={false}
+                aria-hidden
+                className="h-4 w-4 text-zinc-500"
+              />
             </span>
           ) : (
             <Link
               href={createUrl(pathname, 1)}
               aria-label="Go to first page, page 1"
+              className="h-7 w-7 rounded bg-zinc-800 p-1.5 ring-1 ring-inset ring-zinc-700 transition-colors hover:bg-zinc-900"
             >
-              <ChevronsLeftIcon />
+              <ChevronsLeftIcon className="h-4 w-4 text-zinc-100" />
             </Link>
           )}
 
           {isFirstPage ? (
-            <span aria-disabled>
-              <ChevronLeftIcon focusable={false} aria-hidden />
+            <span
+              aria-disabled
+              className="h-7 w-7 rounded bg-zinc-800 p-1.5 opacity-90 ring-1 ring-inset ring-zinc-700"
+            >
+              <ChevronLeftIcon
+                focusable={false}
+                aria-hidden
+                className="h-4 w-4 text-zinc-500"
+              />
             </span>
           ) : (
             <Link
               href={createUrl(pathname, page - 1)}
               aria-label={`Go to previous page, page ${page - 1}`}
+              className="h-7 w-7 rounded bg-zinc-800 p-1.5 ring-1 ring-inset ring-zinc-700 transition-colors hover:bg-zinc-900"
             >
-              <ChevronLeftIcon />
+              <ChevronLeftIcon className="h-4 w-4 text-zinc-100" />
             </Link>
           )}
 
           {isLastPage ? (
-            <span aria-disabled>
-              <ChevronRightIcon focusable={false} aria-hidden />
+            <span
+              aria-disabled
+              className="h-7 w-7 rounded bg-zinc-800 p-1.5 opacity-90 ring-1 ring-inset ring-zinc-700"
+            >
+              <ChevronRightIcon
+                focusable={false}
+                aria-hidden
+                className="h-4 w-4 text-zinc-500"
+              />
             </span>
           ) : (
             <Link
               href={createUrl(pathname, page + 1)}
               aria-label={`Go to next page, page ${page + 1}`}
+              className="h-7 w-7 rounded bg-zinc-800 p-1.5 ring-1 ring-inset ring-zinc-700 transition-colors hover:bg-zinc-900"
             >
-              <ChevronRightIcon />
+              <ChevronRightIcon className="h-4 w-4 text-zinc-100" />
             </Link>
           )}
 
           {isLastPage ? (
-            <span aria-disabled>
-              <ChevronsRightIcon focusable={false} aria-hidden />
+            <span
+              aria-disabled
+              className="h-7 w-7 rounded bg-zinc-800 p-1.5 opacity-90 ring-1 ring-inset ring-zinc-700"
+            >
+              <ChevronsRightIcon
+                focusable={false}
+                aria-hidden
+                className="h-4 w-4 text-zinc-500"
+              />
             </span>
           ) : (
             <Link
               href={createUrl(pathname, totalPages)}
               aria-label={`Go to last page, page ${totalPages}`}
+              className="h-7 w-7 rounded bg-zinc-800 p-1.5 ring-1 ring-inset ring-zinc-700 transition-colors hover:bg-zinc-900"
             >
-              <ChevronsRightIcon />
+              <ChevronsRightIcon className="h-4 w-4 text-zinc-100" />
             </Link>
           )}
         </div>
