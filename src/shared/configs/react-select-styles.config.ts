90c520af6d2b594826a5ffd5a92003e7729d90d8
jhonathanalencar <jhonathan.alencarfs@gmail.com>
2024-05-17 19:57:56 -0300

    feat: add react select

---
+++ src/shared/configs/react-select-styles.config.ts
@@ -0,0 +1,73 @@
+import type {
+  CSSObjectWithLabel,
+  OptionProps,
+  StylesConfig,
+} from 'react-select';
+import colors from 'tailwindcss/colors';
+
+export const styles = {
+  control: (styles: CSSObjectWithLabel) => ({
+    ...styles,
+    backgroundColor: colors.zinc[950],
+    borderColor: colors.zinc[700],
+    ':focus-within': {
+      borderColor: colors.green[200],
+    },
+    ':hover': {
+      borderColor: colors.green[200],
+    },
+  }),
+  menu: (styles: CSSObjectWithLabel) => ({
+    ...styles,
+    backgroundColor: colors.gray[800],
+  }),
+  menuList: (styles: CSSObjectWithLabel) => ({
+    ...styles,
+    maxHeight: '12rem',
+
+    '::-webkit-scrollbar': {
+      width: '0.25rem',
+      height: '0',
+    },
+    '::-webkit-scrollbar-track': {
+      background: colors.zinc[800],
+    },
+    '::-webkit-scrollbar-thumb': {
+      background: colors.blue[600],
+      borderRadius: '0.25rem',
+    },
+    '::-webkit-scrollbar-thumb:hover': {
+      background: colors.blue[700],
+    },
+  }),
+  input: (styles: CSSObjectWithLabel) => ({
+    ...styles,
+    height: '2.5rem',
+    backgroundColor: colors.zinc[950],
+    color: colors.zinc[300],
+  }),
+  option: (styles: CSSObjectWithLabel, { isFocused }: OptionProps) => ({
+    ...styles,
+    backgroundColor: isFocused ? colors.gray[700] : colors.gray[800],
+    color: colors.zinc[300],
+    ':hover': {
+      backgroundColor: colors.zinc[700],
+    },
+  }),
+  singleValue: (styles: CSSObjectWithLabel) => ({
+    ...styles,
+    position: 'relative',
+    color: colors.zinc[200],
+  }),
+  multiValue: (styles: CSSObjectWithLabel) => ({
+    ...styles,
+    position: 'relative',
+    color: colors.zinc[300],
+    backgroundColor: colors.zinc[700],
+  }),
+  multiValueLabel: (styles: CSSObjectWithLabel) => ({
+    ...styles,
+    position: 'relative',
+    color: colors.zinc[300],
+  }),
+} as StylesConfig;
--- src/subdomains/dashboard/components/create-product-dialog.component.tsx
+++ src/subdomains/dashboard/components/create-product-dialog.component.tsx
@@ -1,5 +1,18 @@
 import * as Dialog from '@radix-ui/react-dialog';
+import Select from 'react-select';
 import { XIcon } from 'lucide-react';
+
+import { styles } from '@shared/configs/react-select-styles.config';
+
+const categoriesOptions = [
+  { value: 'headphone', label: 'Headphone' },
+  { value: 'video-games', label: 'Video Games' },
+];
+
+const discountOptions = [
+  { value: '10', label: '10%' },
+  { value: '20', label: '20%' },
+];
 
 export function CreateProductDialog() {
   return (
@@ -22,87 +35,99 @@ export function CreateProductDialog() {
         <Dialog.Description className="mt-1 text-sm text-zinc-400">
           Create a new product here. Click save when you&apos;re done.
         </Dialog.Description>
-        <fieldset className="mt-3 flex flex-col gap-1">
-          <label htmlFor="name" className="font-medium text-zinc-300">
-            Name
-          </label>
-          <input
-            id="name"
-            name="name"
-            defaultValue="Golden Beats Headphones"
-            className="h-10 rounded bg-zinc-950 px-2 font-normal text-zinc-500 outline-none focus-visible:ring-2 focus-visible:ring-green-400 focus-visible:ring-offset-2 focus-visible:ring-offset-zinc-900"
-          />
-        </fieldset>
-        <fieldset className="mt-3 flex flex-col gap-1">
-          <label className="font-medium text-zinc-300" htmlFor="description">
-            Description
-          </label>
-          <input
-            id="description"
-            defaultValue="Beats Studio delivers the perfect blend of design culture, creative culture, and engineering coming together."
-            className="h-10 rounded bg-zinc-950 px-2 font-normal text-zinc-500 outline-none focus-visible:ring-2 focus-visible:ring-green-400 focus-visible:ring-offset-2 focus-visible:ring-offset-zinc-900"
-          />
-        </fieldset>
-        <fieldset className="mt-3 flex flex-col gap-1">
-          <label htmlFor="price" className="font-medium text-zinc-300">
-            Price
-          </label>
-          <input
-            id="price"
-            type="number"
-            defaultValue="30"
-            className="h-10 rounded bg-zinc-950 px-2 font-normal text-zinc-500 outline-none focus-visible:ring-2 focus-visible:ring-green-400 focus-visible:ring-offset-2 focus-visible:ring-offset-zinc-900"
-          />
-        </fieldset>
-        <fieldset className="mt-3 flex flex-col gap-1">
-          <label htmlFor="categories" className="font-medium text-zinc-300">
-            Categories
-          </label>
-          <select
-            id="categories"
-            className="h-10 rounded bg-zinc-950 px-2 font-normal text-zinc-500 outline-none focus-visible:ring-2 focus-visible:ring-green-400 focus-visible:ring-offset-2 focus-visible:ring-offset-zinc-900"
-          >
-            <option value="headphone">Headphones</option>
-          </select>
-        </fieldset>
-        <fieldset className="mt-3 flex flex-col gap-1">
-          <label htmlFor="image-url" className="font-medium text-zinc-300">
-            Image URL
-          </label>
-          <input
-            id="image-url"
-            type="url"
-            defaultValue="https://images.unsplash.com/photo-1545127398-14699f92334b?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            className="h-10 rounded bg-zinc-950 px-2 font-normal text-zinc-500 outline-none focus-visible:ring-2 focus-visible:ring-green-400 focus-visible:ring-offset-2 focus-visible:ring-offset-zinc-900"
-          />
-        </fieldset>
-        <fieldset className="mt-3 flex flex-col gap-1">
-          <label htmlFor="quantity" className="font-medium text-zinc-300">
-            Quantity
-          </label>
-          <input
-            id="quantity"
-            type="number"
-            defaultValue={10}
-            className="h-10 rounded bg-zinc-950 px-2 font-normal text-zinc-500 outline-none focus-visible:ring-2 focus-visible:ring-green-400 focus-visible:ring-offset-2 focus-visible:ring-offset-zinc-900"
-          />
-        </fieldset>
-        <fieldset className="mt-3 flex flex-col gap-1">
-          <label htmlFor="discount" className="font-medium text-zinc-300">
-            Discount
-          </label>
-          <select
-            id="discount"
-            className="h-10 rounded bg-zinc-950 px-2 font-normal text-zinc-500 outline-none focus-visible:ring-2 focus-visible:ring-green-400 focus-visible:ring-offset-2 focus-visible:ring-offset-zinc-900"
-          >
-            <option value="10">10</option>
-          </select>
-        </fieldset>
-        <div className="mt-6 flex justify-end">
-          <button className="rounded bg-green-400 px-6 py-3 text-sm font-bold uppercase text-zinc-900 outline-none hover:bg-green-500 focus-visible:ring-2 focus-visible:ring-green-400 focus-visible:ring-offset-2 focus-visible:ring-offset-zinc-900">
-            Save
-          </button>
-        </div>
+        <form action="">
+          <fieldset className="mt-3 flex flex-col gap-1">
+            <label htmlFor="name" className="font-medium text-zinc-300">
+              Name
+            </label>
+            <input
+              id="name"
+              name="name"
+              defaultValue="Golden Beats Headphones"
+              className="h-10 rounded bg-zinc-950 px-2 font-normal text-zinc-500 outline-none focus-visible:ring-2 focus-visible:ring-green-400 focus-visible:ring-offset-2 focus-visible:ring-offset-zinc-900"
+            />
+          </fieldset>
+          <fieldset className="mt-3 flex flex-col gap-1">
+            <label className="font-medium text-zinc-300" htmlFor="description">
+              Description
+            </label>
+            <input
+              id="description"
+              defaultValue="Beats Studio delivers the perfect blend of design culture, creative culture, and engineering coming together."
+              className="h-10 rounded bg-zinc-950 px-2 font-normal text-zinc-500 outline-none focus-visible:ring-2 focus-visible:ring-green-400 focus-visible:ring-offset-2 focus-visible:ring-offset-zinc-900"
+            />
+          </fieldset>
+          <fieldset className="mt-3 flex flex-col gap-1">
+            <label htmlFor="price" className="font-medium text-zinc-300">
+              Price
+            </label>
+            <input
+              id="price"
+              type="number"
+              defaultValue="30"
+              className="h-10 rounded bg-zinc-950 px-2 font-normal text-zinc-500 outline-none focus-visible:ring-2 focus-visible:ring-green-400 focus-visible:ring-offset-2 focus-visible:ring-offset-zinc-900"
+            />
+          </fieldset>
+          <fieldset className="mt-3 flex flex-col gap-1">
+            <label htmlFor="categories" className="font-medium text-zinc-300">
+              Categories
+            </label>
+            <Select
+              id="product-categories"
+              inputId="categories"
+              isMulti
+              isSearchable
+              name="categories"
+              defaultValue={[categoriesOptions[0]]}
+              options={categoriesOptions}
+              styles={styles}
+            />
+          </fieldset>
+          <fieldset className="mt-3 flex flex-col gap-1">
+            <label htmlFor="image-url" className="font-medium text-zinc-300">
+              Image URL
+            </label>
+            <input
+              id="image-url"
+              type="url"
+              defaultValue="https://images.unsplash.com/photo-1545127398-14699f92334b?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              className="h-10 rounded bg-zinc-950 px-2 font-normal text-zinc-500 outline-none focus-visible:ring-2 focus-visible:ring-green-400 focus-visible:ring-offset-2 focus-visible:ring-offset-zinc-900"
+            />
+          </fieldset>
+          <fieldset className="mt-3 flex flex-col gap-1">
+            <label htmlFor="quantity" className="font-medium text-zinc-300">
+              Quantity
+            </label>
+            <input
+              id="quantity"
+              type="number"
+              defaultValue={10}
+              className="h-10 rounded bg-zinc-950 px-2 font-normal text-zinc-500 outline-none focus-visible:ring-2 focus-visible:ring-green-400 focus-visible:ring-offset-2 focus-visible:ring-offset-zinc-900"
+            />
+          </fieldset>
+          <fieldset className="mt-3 flex flex-col gap-1">
+            <label htmlFor="discount" className="font-medium text-zinc-300">
+              Discount
+            </label>
+            <Select
+              id="discount"
+              inputId="discount"
+              isSearchable
+              name="discount"
+              defaultValue={discountOptions[0]}
+              options={discountOptions}
+              styles={styles}
+            />
+          </fieldset>
+          <div className="mt-6 flex justify-end">
+            <button
+              type="submit"
+              className="rounded bg-green-400 px-6 py-3 text-sm font-bold uppercase text-zinc-900 outline-none hover:bg-green-500 focus-visible:ring-2 focus-visible:ring-green-400 focus-visible:ring-offset-2 focus-visible:ring-offset-zinc-900"
+            >
+              Save
+            </button>
+          </div>
+        </form>
       </Dialog.Content>
     </Dialog.Portal>
   );
