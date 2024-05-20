9be7bd1890e8cf2ff4369352dd3f06e067890489
jhonathanalencar <jhonathan.alencarfs@gmail.com>
2024-05-20 18:35:37 -0300

    feat: add add product query

--- src/subdomains/dashboard/queries.ts
+++ src/subdomains/dashboard/queries.ts
@@ -2,16 +2,6 @@ import 'server-only';
 
 import type { TProduct } from './types';
 
-export async function getProducts(
-  input: GetProductsInput
-): Promise<GetProductsOutput | undefined> {
-  const response = await fetch(
-    `${process.env.CATALOG_API_URL}/products?query=${input.query}&page=${input.page}&limit=${input.limit}`
-  );
-  if (!response.ok) return undefined;
-  return response.json();
-}
-
 type GetProductsInput = {
   page: number;
   limit: number;
@@ -22,3 +12,35 @@ export type GetProductsOutput = {
   total: number;
   products: TProduct[];
 };
+
+export async function getProducts(
+  input: GetProductsInput
+): Promise<GetProductsOutput | undefined> {
+  const response = await fetch(
+    `${process.env.CATALOG_API_URL}/products?query=${input.query}&page=${input.page}&limit=${input.limit}`
+  );
+  if (!response.ok) return undefined;
+  return response.json();
+}
+
+export type AddProductInput = {
+  name: string;
+  description: string;
+  price: number;
+  categories: string[];
+  imageUrl: string;
+  quantity: number;
+  discountId?: string;
+};
+
+export async function addProduct(input: AddProductInput): Promise<void> {
+  const response = await fetch(`${process.env.CATALOG_API_URL}/products`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(input),
+  });
+  if (!response.ok) throw new Error('Error creating user');
+  return response.json();
+}
