c16725668d77f66275695bb29a5de7baf9b422d5
jhonathanalencar <jhonathan.alencarfs@gmail.com>
2024-05-27 18:04:24 -0300

    feat: add fallback ui while the data is being fetched

--- src/subdomains/dashboard/containers/products-edit.container.tsx
+++ src/subdomains/dashboard/containers/products-edit.container.tsx
@@ -1,7 +1,58 @@
+import { Suspense } from 'react';
+import { redirect } from 'next/navigation';
+
 import { getCategories, getDiscounts, getProduct } from '../queries';
+import { getSession } from '@shared/libs/get-session.lib';
 
 import { DashboardLayout } from '@shared/layouts/dashboard.layout';
 import { ProductsEditInterface } from '../interfaces/products-edit.interface';
+import { Skeleton } from '../components/skeleton.component';
+
+interface ProductsEditContainerLoaderProps {
+  productId: string;
+}
+
+export async function ProductsEditContainerLoader({
+  productId,
+}: ProductsEditContainerLoaderProps) {
+  const session = await getSession();
+  if (!session || !session.user) {
+    redirect(`/auth/login?redirect_to=/dashboard/products/${productId}/edit`);
+  }
+  try {
+    const [product, categories, discounts] = await Promise.all([
+      getProduct({ productId }),
+      getCategories(),
+      getDiscounts(),
+    ]);
+
+    return (
+      <ProductsEditInterface
+        product={product}
+        categories={categories}
+        discounts={discounts?.discounts}
+      />
+    );
+  } catch (error) {
+    console.log(error);
+    return (
+      <p className="mt-10 text-center text-xl font-medium text-zinc-200 md:text-2xl">
+        Something went wrong
+      </p>
+    );
+  }
+}
+
+function ProductsEditContainerSkeleton() {
+  return (
+    <section
+      aria-label="Loading product edit"
+      className="mx-auto w-full max-w-7xl p-2 md:p-4"
+    >
+      <Skeleton className="h-[708px]" />
+    </section>
+  );
+}
 
 interface ProductsEditContainerProps {
   params: {
@@ -9,22 +60,12 @@ interface ProductsEditContainerProps {
   };
 }
 
-export async function ProductsEditContainer({
-  params,
-}: ProductsEditContainerProps) {
-  const [product, categories, discounts] = await Promise.all([
-    getProduct(params),
-    getCategories(),
-    getDiscounts(),
-  ]);
-
+export function ProductsEditContainer({ params }: ProductsEditContainerProps) {
   return (
     <DashboardLayout>
-      <ProductsEditInterface
-        product={product}
-        categories={categories}
-        discounts={discounts?.discounts}
-      />
+      <Suspense fallback={<ProductsEditContainerSkeleton />}>
+        <ProductsEditContainerLoader productId={params.productId} />
+      </Suspense>
     </DashboardLayout>
   );
 }
